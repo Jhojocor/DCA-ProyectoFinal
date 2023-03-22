0ce03378bad300c9191148611a3dd246a07e1180
Dilan Cano <98999407+DilanCano@users.noreply.github.com>
2023-03-22 17:07:19 -0500

    Feat Nav

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -1,18 +1,44 @@
-class Nav extends HTMLElement{
-    constructor(){
+class Nav extends HTMLElement {
+    static get observedAttributes(){
+        return["img", "text"];
+    }
+    
+    constructor() {
         super();
-        this.attachShadow({mode: 'open'})
+        this.attachShadow({ mode: "open" });
     }
 
-    connectedCallback(){
+    attributeChangedCallback(propName, oldValue, newValue){
+        this[propName] = newValue;
         this.render();
     }
 
-    render(){
-        this.shadowRoot.innerHTML = `
-        <h2>Navegando ando</h2>
-        `
+    connectedCallback() {
+        var _a;
+        this.render();
+        const btn = (_a = this.shadowRoot) === null || _a === void 0 ? void 0 : _a.querySelector("button");
+        btn === null || btn === void 0 ? void 0 : btn.addEventListener("click", () => {
+            const event = new CustomEvent("create-post", {
+                composed: true
+            });
+            this.dispatchEvent(event);
+        });
+    }
+    render() {
+        if (this.shadowRoot) {
+            this.shadowRoot.innerHTML = `
+            <link rel="stylesheet" href="./styles/nav.css">
+            <link rel="preconnect" href="https://fonts.googleapis.com">
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+            <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500&display=swap" rel="stylesheet">
+
+            <div class="IconText">
+                <img src="${this.img}">
+                <h3>${this.text}</h3>
+            </div>
+                `;
+        }
     }
 }
-
-customElements.define('app-nav', Nav)
+customElements.define("app-nav", Nav);
+export default Nav;
--- src/index.js
+++ src/index.js
@@ -12,17 +12,28 @@ class AppContainer extends HTMLElement {
 
     render(){
         this.shadowRoot.innerHTML = `
-        <app-button></app-button>
-        <app-profile></app-profile>
-        <app-createpost></app-createpost>
-        <app-login></app-login>
-        <app-register></app-register>
-        <app-form></app-form>
-        <app-nav></app-nav>
-        <app-storie></app-storie>
-        <app-suggest></app-suggest>
-        <app-postss></app-postss>
-        <app-home></app-home>
+        <link rel="stylesheet" href="./styles/nav.css">
+        
+        <section id="mainContenedor">
+
+        <section id="NavBar">
+            <img class="Logo" src="https://pbs.twimg.com/media/FrsDBPnXgAEPTV7?format=png&name=240x240">
+            <div class="section">
+                <app-nav img="https://pbs.twimg.com/media/FrsEepdXsAEo2-Z?format=png&name=120x120" text="Home"></app-nav>
+                <app-nav img="https://pbs.twimg.com/media/FrsEhGdX0AAJQiZ?format=png&name=120x120" text="Search"></app-nav>
+                <app-nav img="https://pbs.twimg.com/media/FrsEchHWcAMxoOi?format=png&name=120x120" text="Messages"></app-nav>
+                <app-nav img="https://pbs.twimg.com/media/FrsEeraXgAMNizS?format=png&name=120x120" text="Notifications"></app-nav>
+                <app-nav img="https://pbs.twimg.com/media/FrsEchQXsAAX_KM?format=png&name=120x120" text="New"></app-nav>
+                <app-nav img="https://pbs.twimg.com/media/FrsEer6XsAE7aO0?format=png&name=120x120" text="Profile"></app-nav>
+            </div>
+            <div class="End">
+                <app-nav img="https://pbs.twimg.com/media/FrsEerWWwAIOOnV?format=png&name=120x120" text="Settings"></app-nav>
+            </div>
+        </section>
+
+        </section>
+
+
         `
         //hechos los componentes
     }
