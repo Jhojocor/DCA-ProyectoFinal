f2ed12260db24c76d955328c96e7857e180f8b42
jhojocor <jhojocor@gmail.com>
2023-03-22 19:32:02 -0500

    Feat posts

--- src/components/postss/postss.js
+++ src/components/postss/postss.js
@@ -1,7 +1,16 @@
 class Postss extends HTMLElement{
+    static get observedAttributes(){
+        return["pictureprofile", "user", "ubi", "content", "likes", "description", "coments", "date"];
+    }
+
     constructor(){
         super();
         this.attachShadow({mode: 'open'})
+    }
+
+    attributeChangedCallback(propName, oldValue, newValue){
+        this[propName] = newValue;
+        this.render();
     }
 
     connectedCallback(){
@@ -10,7 +19,42 @@ class Postss extends HTMLElement{
 
     render(){
         this.shadowRoot.innerHTML = `
-        <h2>es un post</h2>
+        <link rel="stylesheet" href="./styles/postss.css">
+
+        <div class="container">
+                <section class="infoContainer">
+                    <div class="userInfo">   
+                        <img src="${this.pictureprofile}" class="profileImg">            
+                        <div class="username"><h2>${this.user} <br> <span>${this.ubi}</span></h2></div>
+                    </div>
+                    <div class="postOptions"><p>•••</p></div>
+                </section>
+                <div class="imgPost"><img src="${this.content}"></div>
+                <section class="icons">
+                    <div class="IgIcons">
+                        <img src="https://pbs.twimg.com/media/FrsEchKWwAEIRjr?format=png&name=120x120">
+                        <img src="https://pbs.twimg.com/media/FrsEchHWcAMxoOi?format=png&name=120x120">
+                        <img src="https://pbs.twimg.com/media/FrsEhGkX0AI7Gge?format=png&name=120x120">
+                    </div>
+                    <div class="carousel">
+                        <div class="grayCircle"></div>
+                        <div class="grayCircle"></div>
+                        <div class="blueCircle"></div>
+                        <div class="grayCircle"></div>
+                        <div class="grayCircle"></div>
+                    </div>
+                    <div class="saveIcon">
+                        <img src="https://pbs.twimg.com/media/FrsEhGIWAAk_bF4?format=png&name=120x120">
+                    </div>
+                </section>
+                <h4 class="counter">${this.likes} likes</h4>
+                <div class="description">
+                    <h4 class="desUser">${this.user}</h4>
+                    <h4 class="desMess">${this.description}</h4>
+                </div>
+                <h5>${this.coments}</h5>
+                <h6>${this.date}</h6>
+            </div>
         `
     }
 }
--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ class AppContainer extends HTMLElement {
         this.shadowRoot.innerHTML = `
         <link rel="stylesheet" href="./styles/nav.css">
         <link rel="stylesheet" href="./styles/storie.css">
+        <link rel="stylesheet" href="./styles/postss.css">
 
         <section id="mainContenedor">
 
@@ -39,7 +40,9 @@ class AppContainer extends HTMLElement {
             <app-storie name="Anuel" img="https://is2-ssl.mzstatic.com/image/thumb/Music112/v4/31/a3/4e/31a34e1a-6b2d-27d7-110d-0db9a9aa42c5/pr_source.png/375x375bb.jpg"></app-storie>
             <app-storie name="Mora" img="https://pbs.twimg.com/profile_images/1509033228874694659/KjCCiVZI_400x400.jpg"></app-storie>
         </article>
-
+        <app-postss pictureprofile="https://anime.atsit.in/es/wp-content/uploads/2023/01/quien-es-dark-deku-y-que-tan-fuerte-es-en-comparacion-con-otros-personajes-de-mha.jpg" user="Jhojocor" ubi="cali, valle" content="https://i.pinimg.com/originals/b6/97/45/b69745e6dde3a0c1065c9d08d8c830be.jpg" likes="50" description="En el concierto de rojuu" coments="Rojuu foreva" date="Friday"></app-postss>
+        <app-postss pictureprofile="https://yt3.ggpht.com/GUW78kIdMM2mVjl-c1rkSD8DqNYSRZAfTUTie3j4xKFN6agTpdb9UiMDGwQB2yuoDpKB1a8QNn8=s900-c-k-c0x00ffffff-no-rj" user="dilancano_" ubi="cali, valle" content="https://www.eluniverso.com/resizer/qGzhwh-7GEsvNWXj_OMmCI5EN2o=/636x670/smart/filters:quality(70)/cloudfront-us-east-1.images.arcpublishing.com/eluniverso/PKSIMOCBHZB7FP4QRQTMHYXYJY.jpg" likes="222" description="La buena fai" coments="viva el ferxxo mor" date="today"></app-postss>
+        <app-postss pictureprofile="https://yt3.ggpht.com/GUW78kIdMM2mVjl-c1rkSD8DqNYSRZAfTUTie3j4xKFN6agTpdb9UiMDGwQB2yuoDpKB1a8QNn8=s900-c-k-c0x00ffffff-no-rj" user="dilancano_" ubi="cali, valle" content="https://caracoltv.brightspotcdn.com/dims4/default/b7ef876/2147483647/strip/false/crop/1500x1000+0+0/resize/1200x800!/quality/90/?url=http%3A%2F%2Fcaracol-brightspot.s3.amazonaws.com%2F94%2F2b%2F206da0a64bd2b69fa432deaa9206%2Ffeid-bogota-mor-movistar-arena.jpg" likes="1550" description="En el concierto de ferxxo" coments="#ferxxo" date="Friday"></app-postss>
         </section>
 
         </section>
