bd158d77495e1fcd89c300c32e3ddf35611b655d
jhojocor <jhojocor@gmail.com>
2023-05-05 11:10:18 -0500

    feat new post

--- src/components/Home/components/Post/postss.ts
+++ src/components/Home/components/Post/postss.ts
@@ -1,61 +1,95 @@
-export default class Postss extends HTMLElement{
+export enum AttributePost {
+    "pictureprofile" = "pictureprofile",
+    "user" = "user",
+    "ubi" = "ubi",
+    "content" = "content",
+    "likes" = "likes",
+    "description" = "description",
+    "coments" = "coments",
+    "date" = "date"
+}
+
+class MyPost extends HTMLElement {
+    pictureprofile?: any;
+    user?: string;
+    ubi?: string;
+    content?: any;
+    likes?: string;
+    description?: string;
+    coments?: string;
+    date?: string;
+
     static get observedAttributes(){
-        return["pictureprofile", "user", "ubi", "content", "likes", "description", "coments", "date"];
+        const attrs: Record<AttributePost,null> = {
+            pictureprofile: null,
+            user: null,
+            ubi: null,
+            content: null,
+            likes: null,
+            description: null,
+            coments: null,
+            date: null
+        };
+        return Object.keys(attrs);
     }
 
     constructor(){
         super();
-        this.attachShadow({mode: 'open'})
+        this.attachShadow({mode: 'open'});
     }
-
-    /*attributeChangedCallback(propName, oldValue, newValue){
-        this[propName] = newValue;
-        this.render();
-    }*/
 
     connectedCallback(){
         this.render();
     }
 
-    render(){
-        if(this.shadowRoot){this.shadowRoot.innerHTML = `
-        <link rel="stylesheet" href="./styles/postss.css">
+    attributeChangedCallback(
+        propName: AttributePost, 
+        oldValue: string | undefined, 
+        newValue: string | undefined){
+        this[propName] = newValue;
+        this.render();
+    }
 
-        <div class="container">
-                <section class="infoContainer">
-                    <div class="userInfo">   
-                        <img src="xd" class="profileImg">            
-                        <div class="username"><h2>xd<br> <span>xd</span></h2></div>
-                    </div>
-                    <div class="postOptions"><p>•••</p></div>
-                </section>
-                <div class="imgPost"><img src="xd"></div>
-                <section class="icons">
-                    <div class="IgIcons">
-                        <img src="https://pbs.twimg.com/media/FrsEchKWwAEIRjr?format=png&name=120x120">
-                        <img src="https://pbs.twimg.com/media/FrsEchHWcAMxoOi?format=png&name=120x120">
-                        <img src="https://pbs.twimg.com/media/FrsEhGkX0AI7Gge?format=png&name=120x120">
-                    </div>
-                    <div class="carousel">
-                        <div class="grayCircle"></div>
-                        <div class="grayCircle"></div>
-                        <div class="blueCircle"></div>
-                        <div class="grayCircle"></div>
-                        <div class="grayCircle"></div>
-                    </div>
-                    <div class="saveIcon">
-                        <img src="https://pbs.twimg.com/media/FrsEhGIWAAk_bF4?format=png&name=120x120">
-                    </div>
-                </section>xd likes</h4>
-                <div class="description">
-                    <h4 class="desUser">xd</h4>
-                    <h4 class="desMess">xd</h4>
+    render(){
+        if(this.shadowRoot){
+            this.shadowRoot.innerHTML = `
+            <link rel="stylesheet" href="./styles/postss.css">
+            <div class="container">
+            <div class="infoContainer">
+                <div class="userInfo">   
+                    <img src="${this.pictureprofile}" class="profileImg">            
+                    <div class="username"><h2>${this.user} <br> <span>${this.ubi}</span></h2></div>
                 </div>
-                <h5>xd</h5>
-                <h6>xd</h6>
+                    <div class="postOptions"><p>•••</p></div>
             </div>
-        `}
+        <div class="imgPost"><img src="${this.content}"></div>
+        <div class="icons">
+        <div class="IgIcons">
+            <img src="https://pbs.twimg.com/media/FrsEchKWwAEIRjr?format=png&name=120x120">
+            <img src="https://pbs.twimg.com/media/FrsEchHWcAMxoOi?format=png&name=120x120">
+            <img src="https://pbs.twimg.com/media/FrsEhGkX0AI7Gge?format=png&name=120x120">
+        </div>
+        <div class="carousel">
+            <div class="grayCircle"></div>
+            <div class="grayCircle"></div>
+            <div class="blueCircle"></div>
+            <div class="grayCircle"></div>
+            <div class="grayCircle"></div>
+        </div>
+        <div class="saveIcon">
+            <img src="https://pbs.twimg.com/media/FrsEhGIWAAk_bF4?format=png&name=120x120">
+        </div></div>
+        <h4 class="counter">${this.likes}</h4>
+        <div class="description">
+            <h4 class="desUser">${this.user}</h4>
+            <h4 class="desMess">${this.description}</h4>
+        </div>
+        <h5>${this.coments}</h5>
+        <h6>${this.date}</h6>
+        </div>`
+        }
     }
 }
 
-customElements.define('app-postss', Postss)
+customElements.define("my-post", MyPost);
+export default MyPost;
