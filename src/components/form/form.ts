7ba98f7ea75deb9f5f513f4456a0a631d818d4d5
Dilan Cano <98999407+DilanCano@users.noreply.github.com>
2023-05-05 13:43:20 -0500

    Feat Form ts and Css

--- src/components/form/form.ts
+++ src/components/form/form.ts
@@ -1,18 +1,55 @@
 export default class Form extends HTMLElement{
+    email = "";
+    password = "";
+
     constructor(){
         super();
-        this.attachShadow({mode: 'open'})
+        this.attachShadow({mode: "open"});
     }
 
     connectedCallback(){
         this.render();
+
+        const btn = this.shadowRoot?.querySelector("button");
+        btn?.addEventListener("click",()=>{
+            const event: CustomEvent<{email:string, password: string}> = 
+            new CustomEvent("form-fullfiled",{
+                detail: {email: this.email, password: this.password},
+                composed: true
+            });
+
+            this.dispatchEvent(event);
+        });
+
+        const emailInput = this.shadowRoot?.querySelector('input[type="email"]');
+        const passwordInput = this.shadowRoot?.querySelector('input[type="password"]');
+        
+        emailInput?.addEventListener("change",(evt) => {
+            const value: string = (evt.target as HTMLInputElement).value || "";
+            this.email = value;
+        });
+
+        passwordInput?.addEventListener("change",(evt) => {
+            const value: string = (evt.target as HTMLInputElement).value || "";
+            this.password = value;
+        })
     }
 
     render(){
-        if(this.shadowRoot){this.shadowRoot.innerHTML = `
-        <h2>Form hexo</h2>
-        `}
+        if(!this.shadowRoot) return;
+        this.shadowRoot.innerHTML = `
+        <article class="Form">
+        <link rel="stylesheet" href="./styles/Form.css">
+            <div>
+                <input name="email" type="email" placeholder="Email"/>
+            </div>
+            <div>
+                <input type="password" placeholder="Password"/>
+            </div>
+            <button type="submit">Send</button>
+        </article>
+        `
     }
 }
 
-customElements.define('app-form', Form)
+customElements.define("app-form",Form);
